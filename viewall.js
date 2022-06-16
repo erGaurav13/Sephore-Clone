dde567151c427c775ff4c8d6386bfc7997b9b2c0
pavan bajait <pbajait@gmail.com>
2022-06-16 12:44:11 +0530

    added login code

---
+++ viewall.js
@@ -0,0 +1,170 @@
+let hairProd=[
+    {
+        src:"https://cdn09.nnnow.com/web-images/medium/styles/NAG35MWAZB5/1603956012732/1.jpg";
+        name: "OLAPLEX";
+        description: "N &#0176 4 Bond Maintenance Shampoo";
+        price: 1,550;      
+        category: "Shampoo" ;  
+    },
+    {
+        src:"https://cdn15.nnnow.com/web-images/medium/styles/KHFROVPTCI2/1599046960541/1.jpg";
+        name: "OLAPLEX";
+        description: "No. 7 Bonding Oil";
+        price: 2,950;      
+        category: "Hair Oil"    
+    },
+    {
+        src:"https://cdn17.nnnow.com/web-images/medium/styles/PSY9GYLUMYU/1634887017760/1.jpg";
+        name: "MOROCCANAOIL";
+        description: "Moisture Repair Shampoo";
+        price: 2,750;       
+        category: "Shampoo" ;   
+    },
+    {
+        src:"https://cdn09.nnnow.com/web-images/medium/styles/626PU93RNYO/1652252610567/1.jpg";
+        name: "QUAI";
+        description: "Leave in conditioner";
+        price: 2,200;    
+        category: "Conditioner" ;        
+    },
+    {
+        src:"https://cdn17.nnnow.com/web-images/medium/styles/PSY9GYLUMYU/1634887017760/1.jpg";
+        name: "MOROCCANAOIL";
+        description: "Texture Clay Hair Cream";
+        price: 2,350;       
+        category: "Hair Styling Products"   
+    },
+    {
+        src:"https://cdn09.nnnow.com/web-images/medium/styles/A6CPZPO5HOS/1634887756893/1.jpg";
+        name: "MOROCCANAOIL";
+        description: "Hydrating Styling Hair Cream";
+        price: 2,700;       
+        category: "Hair Styling Products"   
+    },
+    {
+        src:"https://cdn18.nnnow.com/web-images/medium/styles/E4PUAGOMU8E/1653375921600/1.jpg";
+        name: "MOROCCANAOIL";
+        description: "All in one Leave in Conditioner";
+        price: 3,350;   
+        category: "Conditioner" ;       
+    },
+    {
+        src:"https://cdn19.nnnow.com/web-images/medium/styles/SNX3WCF7MD8/1634886962080/1.jpg";
+        name: "MOROCCANAOIL";
+        description: "Luminous Air Spray Extra Strong";
+        price: 2,160;    
+        category: "Hair Spray";          
+    },
+    {
+        src:"https://sephora.nnnow.com/ouai-detox-shampoo-KR54NI5C40N";
+        name: "QUAI";
+        description: "Detox Shampoo";
+        price: 3,200;  
+        category: "Shampoo" ;        
+    },
+    {
+        src:"https://cdn12.nnnow.com/web-images/medium/styles/QJ2QNWIE87T/1652252637863/1.jpg";
+        name: "MOROCCANAOIL";
+        description: "All in one Leave in Conditioner";
+        price: 3,350;
+        category: "Conditioner" ;          
+    },
+    {
+        src:"https://cdn12.nnnow.com/web-images/medium/styles/QJ2QNWIE87T/1652252637863/1.jpg";
+        name: "QUAI";
+        description: "Wave Spray";
+        price: 3,000; 
+        category: "Hair Spray";        
+    },
+    {
+        src:"https://cdn11.nnnow.com/web-images/medium/styles/NXSX9L9VBBL/1652252632363/1.jpg";
+        name: "QUAI";
+        description: "Mate Pomade";
+        price: 2,400;        
+        category: "Hair Styling Products";  
+    },
+    {
+        src:"https://cdn19.nnnow.com/web-images/medium/styles/R77AWKVN9TK/1633928854562/1.jpg";
+        name: "OLAPLEX";
+        description: "N4 Bond Index Moisture Mask";
+        price: 2,950; 
+        category: "Hair Mask" ;       
+    },
+    {
+        src:"https://cdn06.nnnow.com/web-images/medium/styles/607QU8485GB/1566551690779/1.jpg";
+        name: "SEPHORA COLLECTION";
+        description: "Color Clip Hair Extentions";
+        price: 2,950; 
+        category: "Hair Clip"        
+    },
+    {
+        src:"https://cdn00.nnnow.com/web-images/medium/styles/V73BE2NW20M/1652252643744/1.jpg";
+        name: "QUAI";
+        description: "Volume Spray";
+        price: 2,750; 
+        category: "Hair Spray"        
+    },
+    {
+        src:"https://cdn10.nnnow.com/web-images/medium/styles/J58AWSX9AZI/1586865417826/1.jpg";
+        name: "AVEDA";
+        description: "Control Force Firm Hold Hair Spray";
+        price: 2,050; 
+        category: "Hair Spray"        
+    },
+    {
+        src:"https://cdn07.nnnow.com/web-images/medium/styles/0GM7ARTPJBU/1583131511649/1.jpg";
+        name: "BRIOGEO";
+        description: "Blossom & Bloom Hair Spray";
+        price: 2,200; 
+        category: "Hair Spray"        
+    },
+    {
+        src:"https://cdn04.nnnow.com/web-images/medium/styles/HUI4B96O3UT/1639719970213/1.jpg";
+        name: "ABHATI SUISSE";
+        description: "Chambal Sacred Hair Oil";
+        price: 4,800; 
+        category: "Hair Oil"        
+    },
+    {
+        src:"https://cdn04.nnnow.com/web-images/medium/styles/HUI4B96O3UT/1639719970213/1.jpg";
+        name: "ABHATI SUISSE";
+        description: "Chambal Sacred Hair Oil";
+        price: 4,800; 
+        category: "Hair Oil"        
+    },
+    {
+        src:"https://cdn10.nnnow.com/web-images/medium/styles/J58AWSX9AZI/1586865417826/1.jpg";
+        name: "AVEDA";
+        description: "Nutriplenish Multiuse Hair Oil";
+        price: 2,100; 
+        category: "Hair Oil";   
+    },
+    {
+        src:"https://cdn07.nnnow.com/web-images/medium/styles/0GM7ARTPJBU/1583131511649/1.jpg";
+        name: "BRIOGEO";
+        description: "B.Well Organic Caster Oil";
+        price: 2,200; 
+        category: "Hair Spray"        
+    },
+    {
+        src:"https://cdn01.nnnow.com/web-images/medium/styles/HOOOY8PI2IN/1604639524438/1.jpg";
+        name: "DYSON";
+        description: "40MM Airwrap Long Barrels";
+        price: 3,490; 
+        category: "Hair Styling Products"        
+    },
+    {
+        src:"https://cdn08.nnnow.com/web-images/medium/styles/ZXDKDH6NBRG/1586865417850/1.jpg";
+        name: "AVEDA";
+        description: "Volumizing Tonic";
+        price: 750; 
+        category: "Hair Styling Products"        
+    },
+    {
+        src:"https://cdn19.nnnow.com/web-images/medium/styles/H3RJ9X7RBDD/1491815718951/1.jpg";
+        name: "PERCY & REED";
+        description: "Mini Volumizing Mousse";
+        price: 1,800; 
+        category: "Hair Styling Products"        
+    },
+]
